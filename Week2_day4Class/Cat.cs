188dc2ff70a1df888116725419f56da0aae6a8bf
FaizeChan <obey.faize@gmail.com>
2017-10-05 15:14:36 -0400

    final commit

--- Week2_day4Class/Cat.cs
+++ Week2_day4Class/Cat.cs
@@ -8,5 +8,25 @@ namespace Week2_day4Class
 {
     class Cat
     {
+        //STATES
+        //feilds
+        private string name;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+        //BEHAVIORS
+
+        //constructors
+        public Cat()
+        {
+            //defualt contructor because it takes no
+            //parameters in the parentheses
+        }
     }
 }
